1ffae30d8b3e8eeeca7f121c7d86a6267246be8a
xuedisong <1583477527@qq.com>
2020-03-13 01:32:00 +0800

    优化代码

--- a.py
+++ a.py
@@ -5,10 +5,11 @@ print sys.argv[0]
 b = 0
 print dir()
 f4 = None
+p = 2
 
 
 def f():
-    global c
+    print '李先念:{}'.format(p)
     print dir()
     b = 2
     print 'local..'
@@ -19,6 +20,19 @@ def f():
     print 'f.attr:'
     print dir()
     l12 = [1]
+
+    class F1Class:
+        def __init__(self):
+            self.member = 'member'
+        def mem(self):
+            print 'mem.dir()={}'.format(dir())
+            # print 'mem.__self__={}'.format(__self__)
+
+            print self.member
+        field = 555
+        """zky"""
+        print f4
+        print 'adsfdsgsdgfdssg={}'.format(l12)
 
     def f1():
         global b
@@ -36,7 +50,7 @@ def f():
     print l12
     print local_name
     print dir()
-    return f1
+    return F1Class
 
 
 l1 = [3]
@@ -44,3 +58,17 @@ f()
 f4()
 print 'global..'
 print 'b:', b
+
+
+class Dog:
+    print 'class Dog={}'.format(__module__)
+
+
+def f6():
+    print 'f6={}'.format(dir())
+
+
+f6()
+print dir()
+print p
+print 'a end ........'
--- a1.py
+++ a1.py
@@ -1,4 +1,32 @@
 # -*- coding: utf-8 -*-
 import a
-a.b=100
-print a.b
+from a import p
+
+print 'p={}'.format(p)
+p = 100
+print 'a.p={}'.format(a.p)
+print 'p={}'.format(p)
+cl = a.f()
+instance1=cl()
+print ".....>........"
+print 'dir(cl.mem)={}'.format(dir(cl.mem))
+print 'type(cl.mem)={}'.format(type(cl.mem))
+print 'cl.mem.__self__={}'.format(cl.mem.__self__)
+print 'cl.mem={}'.format(cl.mem)
+
+print 'dir(cl)={}'.format(dir(cl))
+print 'dir(instance1.mem)={}'.format(dir(instance1.mem))
+print 'instance1.mem.__self__={}'.format(instance1.mem.__self__)
+print 'type(instance1.mem)={}'.format(type(instance1.mem))
+print 'instance1.mem={}'.format(instance1.mem)
+instance1.mem()
+cl.mem(instance1)
+print 'dir(instance1)={}'.format(dir(instance1))
+
+instance1.counter = 1
+print 'dir(instance1)={}'.format(dir(instance1))
+while instance1.counter < 10:
+    instance1.counter = instance1.counter * 2
+print 'instance1.counter={}'.format(instance1.counter)
+del instance1.counter
+print 'dir(instance1)={}'.format(dir(instance1))
