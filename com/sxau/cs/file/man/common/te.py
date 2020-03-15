08e6a75259a0b93e3cd8b277111f283b461eeb33
xuedisong <1583477527@qq.com>
2020-03-15 09:32:18 +0800

    优化代码

--- com/sxau/cs/file/man/common/te.py
+++ com/sxau/cs/file/man/common/te.py
@@ -1,8 +1,10 @@
-import com.sxau as a
-import tet
-
-# a.__path__='zky'
-print 'lixin={}'.format(a.__path__)
-
-print dir(tet)
-print 2
+# import com.sxau as a
+# import tet
+#
+# # a.__path__='zky'
+# print 'lixin={}'.format(a.__path__)
+#
+# print dir(tet)
+# print 2
+#  [i for i in range(10)]
+print '\n'.join([' '.join(['{}*{}={}'.format(i, j, i * j) for j in range(1, i + 1)]) for i in range(1, 10)])
